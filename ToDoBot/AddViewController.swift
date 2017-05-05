0b5ea2a9c8f98d477340d65c32ce3697846ceefb
Shun-Ching <Angel@AngeldeMacBook-Pro.local>
2017-05-05 15:13:03 +0800

    add action of navigation bar and coloring navigation bar

---
+++ ToDoBot/AddViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AddViewController.swift
+//  ToDoBot
+//
+//  Created by Shun-Ching, Chou on 2017/5/5.
+//  Copyright © 2017年 Shun-Ching, Chou. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.tintColor = UIColor(red: 137.0/255, green: 203.0/255, blue: 240.0/255, alpha: 1.0)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(saveButton_click))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func saveButton_click(){
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ToDoBot/ViewController.swift
+++ ToDoBot/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        navigationController?.navigationBar.barTintColor = UIColor(red: 88/255, green: 101/255, blue: 167/255, alpha: 1.0)
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
     }
 
     override func didReceiveMemoryWarning() {
